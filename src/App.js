2e144bcbc8288c5e0f92444c75c432ec006fa195
AndMuzych <andrey.muzychenko.99@gmail.com>
2022-07-13 14:26:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+import pizzas from './assets/pizzas.json';
+import { useEffect, useState } from 'react';
+import Skeleton from './components/PizzaBlock/Skeleton';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://62cc9adba080052930adeb53.mockapi.io/pizzas')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {isLoading
+              ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
